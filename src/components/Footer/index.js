b1c57ba9584da683f50fa2757bd1ba1a07fcaf83
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-06-17 12:42:42 +0200

    Add footer styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,38 +3,38 @@ import "./styles.scss";
 
 const Footer = props => {
   return (
-    <div claassName="footer__container">
-        <nav>
-      <ul>
-        <li>
-          <a target="_blank" href="tel:+34656498787">
-            Teléfono: 656498787
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="mailto:marinarodriguezandres@gmail.com">
-            Email: marinarodriguezandres@gmail.com
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://github.com/marinarodriguez">
-            GitHub: marinarodriguez
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/marinarodriguezandres/"
-          >
-            Linkedin: marinarodriguezandres
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://twitter.com/Qoqoconqu">
-            Twitter: @qoqoconqu
-          </a>
-        </li>
-      </ul>
+    <div className="footer__container">
+      <nav>
+        <ul>
+          <li>
+            <a target="_blank" href="tel:+34656498787">
+              <i class="fas fa-phone-alt" /> 656498787
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="mailto:marinarodriguezandres@gmail.com">
+              <i class="far fa-envelope" /> marinarodriguezandres@gmail.com
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com/marinarodriguez">
+              <i class="fab fa-github" /> marinarodriguez
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/marinarodriguezandres/"
+            >
+              <i class="fab fa-linkedin-in" /> marinarodriguezandres
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://twitter.com/Qoqoconqu">
+              <i class="fab fa-twitter" /> @qoqoconqu
+            </a>
+          </li>
+        </ul>
       </nav>
     </div>
   );
