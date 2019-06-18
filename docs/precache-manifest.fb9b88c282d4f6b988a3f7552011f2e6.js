bf816b2bfc8894b35c0d3220d7bc151e63fc05be
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-06-18 17:36:10 +0200

    build docs

---
+++ docs/precache-manifest.fb9b88c282d4f6b988a3f7552011f2e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06b93bdc41fe83d9d3f78d9ac99878c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07e168c096cb08766baa",
+    "url": "./static/css/main.6ee0fdb9.chunk.css"
+  },
+  {
+    "revision": "7b24e97b4d9c12ff1f05",
+    "url": "./static/js/2.3b34fb11.chunk.js"
+  },
+  {
+    "revision": "07e168c096cb08766baa",
+    "url": "./static/js/main.f1e00d16.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
